e757b0197172144d01aaf3ed815f8698349015fb
deepzS2 <alanbackman10@hotmail.com>
2022-07-04 12:41:33 -0300

    🔨: Updating configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import '@/styles/tailwind.css'
+import '@/styles/global.css'
 import '@/styles/styles.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-	return <div>Hello World!</div>
+	return <h1 className="underline text-3xl">Hello World!</h1>
 }
 
 export default Home
