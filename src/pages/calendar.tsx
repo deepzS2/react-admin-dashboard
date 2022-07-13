4f3573771526ddd5755dfcff74ca0f3adfe69759
deepzS2 <alanbackman10@hotmail.com>
2022-07-13 11:07:59 -0300

    ✨: Calendar page

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,11 +1,34 @@
 import { NextPage } from 'next'
 import React from 'react'
 
+import { scheduleData } from '@/data/dummy'
+import { Header } from '@components/index'
+import {
+	ScheduleComponent,
+	Day,
+	Week,
+	WorkWeek,
+	Month,
+	Agenda,
+	Inject,
+	Resize,
+	DragAndDrop,
+} from '@syncfusion/ej2-react-schedule'
+
 const Calendar: NextPage = () => {
 	return (
-		<>
-			<h1>Calendar</h1>
-		</>
+		<div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+			<Header category="App" title="Calendar" />
+			<ScheduleComponent
+				height="650px"
+				eventSettings={{ dataSource: scheduleData }}
+				selectedDate={new Date(2021, 0, 10)}
+			>
+				<Inject
+					services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+				/>
+			</ScheduleComponent>
+		</div>
 	)
 }
 
